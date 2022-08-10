c08bef8ba24636a96c6d72ef243ff429ad32481c
Kushagra Goel <64742426+A223D@users.noreply.github.com>
2022-08-09 22:46:56 -0400

    Implement if statement in begin and incomplete README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # This library helps [AdafruitGFX](https://learn.adafruit.com/adafruit-gfx-graphics-library) work on the [4.01 inch colour e-paper display by WaveShare](https://www.waveshare.com/4.01inch-e-Paper-F.htm)
-> Might work on the [5.65 inch version](https://www.waveshare.com/5.65inch-e-Paper-F.htm)as well. Haven't tested or developed with that in mind. YMMV.
+> Might work on the [5.65 inch version](https://www.waveshare.com/5.65inch-e-Paper-F.htm) as well. Haven't tested or developed with that in mind. YMMV.
+
+**Due to the slow refresh rate of this screen, the usage of this library is slightly different compared to those which support screens with higher refresh rates**
+## Theory
+Both of the screens use SPI to communicate(Controller-in-Peripheral-Out pin does not exist) and receive data to display on the screen. 
+
+SPI can either be explicitly initialized(HSPI is used) or can be left up to the library to do, depending on the `begin()` function used. If left up to the library, default pins for HSPI are used for clock, controller-out-peripheral-in. Check 
+
+For a deeper dive into how this display works, [check this out](https://github.com/A223D/colourEpaperWaveshare). 
+
+## Constructor
+```c++
+Adafruit_4_01_ColourEPaper(int w, int h, int rst_pin, int dc_pin, int busy_pin, bool debug_On);
+```
+This constructor is used to create the object which represents the screen. You can call functions on this object to interact with the screen. It takes the width and height (in pixels), the MCU pin to which the `reset` pin of the screen is connected to, the MCU pin to which the `DC` pin of the screen is connected to, the MCU pin to which the `Busy` pin of the screen is connected to, and a `bool` which enables or disables debugging statements during operation. 
+
+## Functions
+* **Begin (two definitions)**
+	```c++
+	bool begin(SPIClass *spi_ptr, int cs_pin);
+	```
+	The above declaration expects an SPI bus on the ESP32 to be initialized and then it's object to be passed to it, along with MCU pin which is connected to the in `CS` pin on the screen. 
+
--- examples/customPins/customPins.ino
+++ examples/customPins/customPins.ino
@@ -2,71 +2,76 @@
 
 #include <Fonts/FreeSansBold24pt7b.h>
 
-#define RST_PIN         26
-#define DC_PIN          27
-#define BUSY_PIN        25
+#define RST_PIN 26
+#define DC_PIN 27
+#define BUSY_PIN 25
 
-#define HOWMUCH   71
+#define HOWMUCH 71
 
-#define WIDTH           640
-#define HEIGHT          400
+#define WIDTH 640
+#define HEIGHT 400
 
-#define HSPI_COPI   13
-#define HSPI_SCLK   14
-#define HSPI_SS     15
+#define HSPI_COPI 13
+#define HSPI_SCLK 14
+#define HSPI_SS 15
 
 Adafruit_4_01_ColourEPaper ePaperObject(WIDTH, HEIGHT, RST_PIN, DC_PIN, BUSY_PIN, false);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  ePaperObject.begin(HSPI_SCLK, HSPI_COPI, HSPI_SS);
+  if (!ePaperObject.begin(HSPI_SCLK, HSPI_COPI, HSPI_SS))
+  {
+    Serial.println("Something went wrong. Enable debugging and check");
+  }
 
   Serial.println("Let's clear the screen");
   ePaperObject.display();
   ePaperObject.waitForScreenBlocking();
 
   Serial.println("Blank Screen now");
-  delay(10000);                           //wait for 10 seconds
+  delay(10000); // wait for 10 seconds
 
   apparentlyFunction();
   ePaperObject.display();
-  while(ePaperObject.checkBusy());
+  while (ePaperObject.checkBusy())
+    ;
   ePaperObject.sendPOFandLeaveSPI();
 
   Serial.println("Done!");
 }
 
-void loop() {
+void loop()
+{
 
   // Nothing here
-
 }
 
-
-void apparentlyFunction() {
+void apparentlyFunction()
+{
   ePaperObject.setFont(&FreeSansBold24pt7b);
-  ePaperObject.setTextSize(1);           // Normal 1:1 pixel scale
-  ePaperObject.setTextColor(EPD_COLOUR_BLACK);        // Draw black text
-  ePaperObject.setCursor(52, 33);            // Start at top-left corner
+  ePaperObject.setTextSize(1);                 // Normal 1:1 pixel scale
+  ePaperObject.setTextColor(EPD_COLOUR_BLACK); // Draw black text
+  ePaperObject.setCursor(52, 33);              // Start at top-left corner
   ePaperObject.println("Apparently this works...");
 
-  ePaperObject.setTextColor(EPD_COLOUR_GREEN);        // Draw green text
+  ePaperObject.setTextColor(EPD_COLOUR_GREEN); // Draw green text
   ePaperObject.setCursor(358, 33 + (HOWMUCH * 1));
   ePaperObject.println("And this...");
 
-  ePaperObject.setTextColor(EPD_COLOUR_BLUE);        // Draw blue text
+  ePaperObject.setTextColor(EPD_COLOUR_BLUE); // Draw blue text
   ePaperObject.setCursor(358, 33 + (HOWMUCH * 2));
   ePaperObject.println("And this...");
 
-  ePaperObject.setTextColor(EPD_COLOUR_RED);        // Draw red text
+  ePaperObject.setTextColor(EPD_COLOUR_RED); // Draw red text
   ePaperObject.setCursor(358, 33 + (HOWMUCH * 3));
   ePaperObject.println("And this...");
 
-  ePaperObject.setTextColor(EPD_COLOUR_YELLOW);        // Draw yellow text
+  ePaperObject.setTextColor(EPD_COLOUR_YELLOW); // Draw yellow text
   ePaperObject.setCursor(358, 33 + (HOWMUCH * 4));
   ePaperObject.println("And this...");
 
-  ePaperObject.setTextColor(EPD_COLOUR_ORANGE);        // Draw orange text
+  ePaperObject.setTextColor(EPD_COLOUR_ORANGE); // Draw orange text
   ePaperObject.setCursor(358, 33 + (HOWMUCH * 5));
   ePaperObject.println("And this...");
 }
